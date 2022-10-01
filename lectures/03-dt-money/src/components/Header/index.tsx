48ce0650bcbd669e8cb8dd65b39a18a2e773a906
LucasFell <lucas.fell@partner.bmw.com.br>
2022-10-01 18:20:27 -0300

    feat: add accessible dialog

--- lectures/03-dt-money/src/components/Header/index.tsx
+++ lectures/03-dt-money/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
-
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
 
 export function Header() {
@@ -8,7 +8,18 @@ export function Header() {
       <HeaderContent>
         <img alt="" src={logoImg} />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
