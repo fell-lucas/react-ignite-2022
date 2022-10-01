9294eb9ad8f122737f05983c0fa9136686b3a71f
LucasFell <lucas.fell@partner.bmw.com.br>
2022-10-01 19:05:25 -0300

    feat: add radix radio group

--- lectures/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ lectures/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from 'phosphor-react';
-import { CloseButton, Content, Overlay } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles';
 
 export function NewTransactionModal() {
   return (
@@ -18,6 +24,17 @@ export function NewTransactionModal() {
           <input placeholder="Descrição" required type="text" />
           <input placeholder="Preço" required type="number" />
           <input placeholder="Categoria" required type="text" />
+
+          <TransactionType>
+            <TransactionTypeButton value="income" variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton value="outcome" variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
