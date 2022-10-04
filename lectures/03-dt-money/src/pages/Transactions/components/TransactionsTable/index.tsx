5fe32c0f439498c9836935da6cca74b7bb0f2036
LucasFell <lucas.fell@partner.bmw.com.br>
2022-10-04 18:30:12 -0300

    feat: load transactions from mock server

--- lectures/03-dt-money/src/pages/Transactions/components/TransactionsTable/index.tsx
+++ lectures/03-dt-money/src/pages/Transactions/components/TransactionsTable/index.tsx
@@ -1,25 +1,41 @@
+import { useEffect, useState } from 'react';
 import { StyledTransactionsTable, PriceHighlight } from './styles';
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  category: string;
+  price: number;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions');
+    const data = (await response.json()) as Transaction[];
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    void loadTransactions();
+  }, []);
+
   return (
     <StyledTransactionsTable>
       <tbody>
-        <tr>
-          <td width="50%">Desenvolvimento de site</td>
-          <td>
-            <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-          </td>
-          <td>Venda</td>
-          <td>13/04/2022</td>
-        </tr>
-        <tr>
-          <td width="50%">Hambúrguer</td>
-          <td>
-            <PriceHighlight variant="outcome">-R$ 59,00</PriceHighlight>
-          </td>
-          <td>Alimentação</td>
-          <td>10/04/2022</td>
-        </tr>
+        {transactions.map((t) => (
+          <tr key={t.id}>
+            <td width="50%">{t.description}</td>
+            <td>
+              <PriceHighlight variant={t.type}>{t.price}</PriceHighlight>
+            </td>
+            <td>{t.category}</td>
+            <td>{t.createdAt}</td>
+          </tr>
+        ))}
       </tbody>
     </StyledTransactionsTable>
   );
