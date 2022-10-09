60675b50db142c92a663a9470f5a8c9efa2e9923
Lucas Alexandre Fell <lucasafell@gmail.com>
2022-10-09 19:38:08 -0300

    feat: react-hook-form and zod

--- lectures/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ lectures/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -7,8 +7,33 @@ import {
   TransactionType,
   TransactionTypeButton,
 } from './styles';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  // type: z.enum(['income', 'outcome']),
+});
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+  });
+
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+
   return (
     <Dialog.Portal>
       <Overlay />
@@ -19,11 +44,21 @@ export function NewTransactionModal() {
         <CloseButton>
           <X size={24} />
         </CloseButton>
-
-        <form>
-          <input placeholder="Descrição" required type="text" />
-          <input placeholder="Preço" required type="number" />
-          <input placeholder="Categoria" required type="text" />
+        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            placeholder="Descrição"
+            required
+            type="text"
+            {...register('description')}
+          />
+          <input
+            placeholder="Preço"
+            required
+            type="number"
+            {...register('price', { valueAsNumber: true })}
+          />
+          <input placeholder="Categoria" required type="text" {...register('category')} />
 
           <TransactionType>
             <TransactionTypeButton value="income" variant="income">
@@ -36,7 +71,9 @@ export function NewTransactionModal() {
             </TransactionTypeButton>
           </TransactionType>
 
-          <button type="submit">Cadastrar</button>
+          <button disabled={isSubmitting} type="submit">
+            Cadastrar
+          </button>
         </form>
       </Content>
     </Dialog.Portal>
--- lectures/03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ lectures/03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,33 @@
 import { MagnifyingGlass } from 'phosphor-react';
 import { SearchFormContainer } from './styles';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const searchFormSchema = z.object({
+  query: z.string(),
+});
+
+type SearchFormInput = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input placeholder="Busque por transações" type="text" />
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInput>({ resolver: zodResolver(searchFormSchema) });
 
-      <button type="submit">
+  async function handleSearchTransactions(data: SearchFormInput) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input placeholder="Busque por transações" type="text" {...register('query')} />
+
+      <button disabled={isSubmitting} type="submit">
         <MagnifyingGlass size={20} />
         Buscar
       </button>
