40624f49098a55e6a9700f3a19b0974781bc7e88
Lucas Alexandre Fell <lucasafell@gmail.com>
2022-10-16 15:28:32 -0300

    feat: add json server online

--- lectures/03-dt-money/src/lib/axios.ts
+++ lectures/03-dt-money/src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3333'
+      : 'https://my-json-server.typicode.com/fell-lucas/react-ignite-2022-dt-money-db-pt/',
 });
